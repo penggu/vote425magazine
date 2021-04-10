554f94239e8587a7d4fc7c334619fe888128849b
Peng Gu <penggu@gmail.com>
2021-04-10 14:36:37 -0700

    add support for cancel
    - locate time slot in calendar using text instead of index
    - remove deprecated code

--- book_north_west_badminton.py
+++ book_north_west_badminton.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 import time
 from datetime import datetime
-import random
 from nwb_data import zen_planner
 import calendar
 import json
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 
 def print_dbg(msg):
@@ -38,7 +36,6 @@ def seconds_before_day_time(day, hour):
     now: datetime = datetime.now()
     hour_at_today = datetime(now.year, now.month, now.day, hour, 0, 0)
     delta_hour = hour_at_today - now
-    # debug(f'now is {now}, {seconds_to_duration(delta_hour.total_seconds())} before today at {hour}:00:00')
     delta_days = (day - now.weekday()) % 7
     ans = delta_days * 24 * 60 * 60 + delta_hour.total_seconds()
     return ans
@@ -51,17 +48,19 @@ def get_delay_in_seconds():
     else:
         wait for seconds until half way to Saturday 10am
     """
-    min_delay = 300  # seconds
-
     today = datetime.today()
     weekday = today.weekday()
     current_hour = today.hour
 
     delay = seconds_before_saturday_10am() / 2  # TODO: choose this one during weekend
     # delay = seconds_before_wednesday_4pm() / 2  # TODO: choose this one during weekday
-    print_dbg(f'seconds before next booking = {delay}')
+    print_dbg(f'seconds before next booking window = {delay}')
 
-    return max(delay, min_delay)  # this will make sure delay is a positive number
+    min_delay = 30  # this will make sure delay is a positive number
+    max_delay = 300  # this will make sure browser login does not expire
+    delay = max(delay, min_delay)
+    delay = min(delay, max_delay)
+    return delay
 
 
 def seconds_to_duration(seconds):
@@ -85,88 +84,6 @@ def days_before_next(target_day):
         days_before_target_day += 7
     print_dbg(f'There are {days_before_target_day} days before next {target_day}')
     return days_before_target_day
-
-
-# def book_single_user_single_slot_end_to_end(user, day, slot):
-#     booked = False
-#
-#     # launch browser
-#     browser = webdriver.Firefox()
-#     browser.implicitly_wait(0.5)
-#
-#     try:
-#         # navigate to login page
-#         browser.get(zen_planner['login']['url'])
-#         title_text = zen_planner['login']['title']
-#         debug(f'{title_text} in {browser.title}')
-#         assert title_text in browser.title
-#
-#         # login
-#         email_text_box = browser.find_element_by_id('idUsername')
-#         email_text_box.send_keys(zen_planner['login']['email'])
-#
-#         password_text_box = browser.find_element_by_id('idPassword')
-#         password_text_box.send_keys(zen_planner['login']['password'])
-#
-#         submit_button = browser.find_element_by_xpath('/html/body/div[1]/table/tbody/tr/td[2]/fieldset['
-#                                                       '1]/div/form/input')
-#         submit_button.click()
-#
-#         # navigate to calendar page
-#         browser.get(zen_planner['bookings']['url'])
-#
-#         # forward all the way to the target date in calendar
-#         days_later = days_before_next(day)
-#         for _ in range(days_later):
-#             next_day_button = browser.find_element_by_xpath('/html/body/div/table/tbody/tr/td[2]/table[1]/tbody/tr/td['
-#                                                             '1]/div/a[3]/i')
-#             next_day_button.click()
-#
-#         # click on the time slot
-#         slot_button = browser.find_element_by_xpath(f'/html/body/div/table/tbody/tr/td[2]/table[2]/tbody/tr['
-#                                                     f'{get_slot_number_on_day(day, slot)}]/td[1]/div')
-#         slot_button.click()
-#
-#         # select family member
-#         family_member_select_box = Select(browser.find_element_by_id('familyMembers'))
-#         family_member_select_box.select_by_visible_text(user)
-#
-#         # book time slot for selected family member
-#         try:
-#             reserve_button = browser.find_element_by_id('reserve_1')
-#             reserve_button.click()
-#         except Exception as e:
-#             debug(f'Failed to locate Reserve button. {e}')
-#
-#         # done, we should be able to see the cancel option now
-#
-#         cancel_button = browser.find_element_by_xpath('//a[contains(@id, "cancel_")]')  # *[@id="cancel_2"]
-#         # cancel_button.click()  # TODO: delete this line after debugging
-#         booked = True
-#         info(f'Successfully booked for {user} on {day} at {slot}')
-#
-#     except Exception as e:
-#         info(f'Failed to book for {user} on {day} at {slot}')
-#         debug(f'Exception occurred: {e}')
-#
-#     browser.quit()
-#     return booked
-
-
-# def book_court_2(booked=[]):
-#     all_success = True
-#     for day in zen_planner['bookings']['time_slots'].keys():
-#         slots = zen_planner['bookings']['time_slots'][day]
-#         for slot in slots:
-#             for username in zen_planner['bookings']['users'].keys():
-#                 booking = [username, day, slot]
-#                 if booking not in booked:
-#                     info(f'Booking for {username} on {day} at {slot}')
-#                     success = book_single_user_single_slot_end_to_end(username, day, slot)
-#                     all_success = success and all_success
-#                     if success:
-#                         booked.append(booking)
-#     return all_success
 
 
 def login():
@@ -202,32 +119,20 @@ def login():
     return browser
 
 
-# def is_weekday(day):
-#     ans = list(calendar.day_name).index(day) < 5
-#     print_dbg(f'{day} is a ' + ('weekday' if ans else 'weekend'))
-#     return ans
-
-
-# def get_slot_number_on_day(day, slot):
-#     if is_weekday(day):
-#         slot_number = zen_planner['bookings']['slot_numbers']['Weekday'][slot]
-#     else:
-#         slot_number = zen_planner['bookings']['slot_numbers']['Weekend'][slot]
-#     return slot_number
-
-
 def get_slot_button_on_day(day, slot, browser):
-    for row_number in range(20):  # scan all possible calendar rows on the day
-        try:  # an exception will occur if a row number does not exist in the calendar
-            slot_button = browser.find_element_by_xpath(f'/html/body/div/table/'
-                                                        f'tbody/tr/td[2]/table[2]/tbody/tr[{row_number}]/td[1]/div')
-        except NoSuchElementException as e:  # we don't care if an row does not exist
-            pass
-        else:
-            if slot_button.text == slot.upper():   # make sure it is the right row by matching the text
-                return slot_button
-    print_dbg(f'{slot} on {day} is not open yet, please wait until it shows up.')
-    return None
+    while True:
+        for row_number in range(20):  # scan all possible calendar rows on the day
+            try:  # an exception will occur if a row number does not exist in the calendar
+                slot_button = browser.find_element_by_xpath(f'/html/body/div/table/'
+                                                            f'tbody/tr/td[2]/table[2]/tbody/tr[{row_number}]/td[1]/div')
+            except NoSuchElementException as e:  # we don't care if an row does not exist
+                pass
+            else:
+                if slot_button.text == slot.upper():   # make sure it is the right row by matching the text
+                    return slot_button
+        print_dbg(f'{slot} on {day} is not open yet, keep trying until it shows up.')
+        time.sleep(3)
+        browser.refresh()
 
 
 def book_single_user_single_slot_with_browser(user, day, slot, browser):
@@ -252,7 +157,7 @@ def book_single_user_single_slot_with_browser(user, day, slot, browser):
         family_member_select_box = Select(browser.find_element_by_id('familyMembers'))
         family_member_select_box.select_by_visible_text(user)
 
-        time.sleep(3)
+        time.sleep(2)
 
         # book time slot for selected family member
         try:
@@ -263,13 +168,20 @@ def book_single_user_single_slot_with_browser(user, day, slot, browser):
 
         # done, we should be able to see the cancel option now
         try:
+            action = 'book'  # default behavior is to reserve
+            if zen_planner['bookings']['action'] == 'cancel':
+                action = 'cancel'
+
             cancel_button = browser.find_element_by_xpath('//a[contains(@id, "cancel_")]')  # *[@id="cancel_2"]
-            # cancel_button.click()  # TODO: delete this line
-            print_inf(f'Successfully booked for {user} on {day} at {slot}')
+
+            if zen_planner['bookings']['action'] == 'cancel':
+                cancel_button.click()
+
+            print_inf(f'Successfully {action}ed for {user} on {day} at {slot}')
             success = True
         except NoSuchElementException as e:
             print_dbg(f'Cancel button is not available.')
-            print_err(f'Failed to book for {user} on {day} at {slot}')
+            print_err(f'Failed to {action} for {user} on {day} at {slot}')
 
     except Exception as e:
         print_dbg(f'Unexpected exception occurred: {str(e).rstrip()}')
@@ -277,10 +189,7 @@ def book_single_user_single_slot_with_browser(user, day, slot, browser):
     return success
 
 
-def book_court(booked=[]):
-    # login
-    browser = login()
-
+def book_court(booked, browser):
     # for each slot, for each person, try booking
     all_success = True
     for day in zen_planner['bookings']['time_slots'].keys():
@@ -294,34 +203,32 @@ def book_court(booked=[]):
                     all_success = success and all_success
                     if success:
                         booked.append(booking)
+    return all_success
+
+
+def book_for_all():
+    booked = []
+
+    # login
+    browser = login()
+
+    success = False
+    while not success:
+        success = book_court(booked, browser)
+        print_inf(f'\nbooked = {booked}')
+        if success:
+            break
+        delay = get_delay_in_seconds()
+        print_inf(f'\n==================================================\n'
+                  f'waiting to try again in {seconds_to_duration(delay)}\n')
+        time.sleep(delay)
 
     # close browser
     browser.quit()
 
-    return all_success
-
 
 def main():
-    with open('booked.json', 'r') as log_file:
-        booked = json.load(log_file)
-        print_dbg(f'01 booked = {booked}')
-
-    success = book_court(booked)
-    with open('booked.json', 'w') as log_file:
-        print_dbg(f'02 booked = {booked}')
-        json.dump(booked, log_file)
-
-    while not success:
-        delay = get_delay_in_seconds()
-        print_dbg(f'==================================================\n'
-                  f'waiting to try again in {seconds_to_duration(delay)}\n')
-        time.sleep(delay)
-        success = book_court(booked)
-        with open('booked.json', 'w') as log_file:
-            print_dbg(f'03 booked = {booked}')
-            json.dump(booked, log_file)
-
-    return
+    book_for_all()
 
 
 if __name__ == '__main__':
--- nwb_data_anonymous.py
+++ nwb_data_anonymous.py
@@ -6,6 +6,7 @@ zen_planner = dict(
         password='top_secret',
     ),
     bookings=dict(
+        action='reserve',  # either 'reserve' or 'cancel'
         url='https://northwestbadmintonacademy.sites.zenplanner.com/calendar.cfm',
         users={
             'Firstname1 Lastname1': 0,
@@ -15,17 +16,6 @@ zen_planner = dict(
             Monday=['8:00 am'],
             Tuesday=['9:00 am'],
             Sunday=['8:00 pm']
-        ),
-        slot_numbers=dict(
-            Weekday={
-                '2:00 pm': 2, '3:00 pm': 3, '4:00 pm': 4,  # 2:00 pm is start of day
-                '5:00 pm': 5, '6:00 pm': 6, '7:00 pm': 7, '8:00 pm': 8, '9:00 pm': 9, '10:00 pm': 10  # 10:00 pm is end of day
-            },
-            Weekend={
-                '8:00 am': 2, '9:00 am': 3, '10:00 am': 4, '11:00 am': 5,  # 8:00 am is start of day
-                '12:00 pm': 6, '1:00 pm': 7, '2:00 pm': 8, '3:00 pm': 9, '4:00 pm': 10,
-                '5:00 pm': 11, '6:00 pm': 12, '7:00 pm': 13, '8:00 pm': 14, '9:00 pm': 15, '10:00 pm': 16  # 10:00 pm is end of day
-            },
         ),
     ),
 )
